e7a6d6a4ad54c09fba3078ab7aff671cbc3b2d27
Ciaran <ciaranob.cob@gmail.com>
2024-05-19 21:40:47 +0100

    Add color picker script

---
+++ config/hypr/scripts/ColorPicker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## Color picker ##
+
+MAKOCONFIG=$HOME/.config/mako/config
+result=$(hyprpicker -an)
+tmp=$(mktemp)
+
+# Swap second occurance of border-color
+awk '/border-color=/ && ++count == 2 {sub(/border-color=.*/, "border-color='"$result"'")} 1' "$MAKOCONFIG" > "$tmp" && mv "$tmp" "$MAKOCONFIG"
+makoctl reload
+notify-send -u "normal" "$result Copied to clipboard"
