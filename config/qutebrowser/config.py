143dc85e1e5972c9a55d3b9cde357da0304b30d4
KKV9 <KKV9@noreply.github.com>
2024-06-13 14:02:28 +0100

    Add permissions files to qutebrowser
    Feat: cookies disabled by default

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -5,6 +5,8 @@
 import pywalQute.draw
 import os.path
 
+home = str(os.environ['HOME'])
+
 ## Appearance ##
 c.colors.webpage.preferred_color_scheme = "dark"
 c.colors.webpage.darkmode.enabled = True
@@ -15,18 +17,31 @@ c.scrolling.bar = "never"
 #######
 
 ## Privacy & Adblock ##
-c.content.cookies.accept = "no-unknown-3rdparty"
+c.content.cookies.accept = "never"
 c.content.blocking.method = "both"
 c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
-c.content.cache.size = 500 # enable cache, common in browsers
-c.content.webgl = False # Disable webgl finger printing
-c.content.canvas_reading = False # Disable canvas finger printing
-config.set("content.javascript.clipboard", "access", "https://github.com/*") # Allow clipboard access
+c.content.cache.size = 500  # enable cache, common in browsers
+c.content.webgl = False  # Disable webgl finger printing
+c.content.canvas_reading = False  # Disable canvas finger printing
+#######
+
+## Permissions ##
+with open(f'{home}/.config/qutebrowser/permissions/cookies') as f:
+    for site in f:
+        config.set("content.cookies.accept", "no-3rdparty",
+                   f"{site}")  # Allow cookies
+
+with open(f'{home}/.config/qutebrowser/permissions/clipboard') as f:
+    for site in f:
+        config.set("content.javascript.clipboard", "access",
+                   f"{site}")  # Allow clipboard access
 #######
 
 ## Binds ##
-config.bind(',m', 'spawn mpv {url}') # Open mpv
-config.bind(',t', 'open -t https://translate.google.com/translate?sl=auto&tl=en&u={url}') # Translate page to english (google)
+config.bind(',m', 'spawn mpv {url}')  # Open mpv
+# Translate page to english (google)
+config.bind(
+    ',t', 'open -t https://translate.google.com/translate?sl=auto&tl=en&u={url}')
 #######
 
 ## Search ##
